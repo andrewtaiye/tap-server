79b71186fe7718fecbbae341ad3e91ceef3c5d6d
Andrew Tai <andrewtai.ye@gmail.com>
2022-12-14 00:20:45 +0800

    Updated: Changed query to include scenario completion %

--- controllers/instructorController.ts
+++ controllers/instructorController.ts
@@ -26,15 +26,22 @@ const getTrainees = async (req: InstructorRequest, res: Response) => {
 
     // Retrieve all trainees
     let query = `
+      WITH scenario_completion AS (
+        SELECT ROUND(SUM(scenario_requirements.fulfilled)::NUMERIC / SUM(scenario_requirements.requirement)::NUMERIC * 100) percentage, user_position_id
+        FROM scenario_requirements
+        GROUP BY user_position_id
+      )
       SELECT user_positions.id AS user_position_id, user_positions.user_id, user_positions.position, user_positions.start_date, user_positions.cat_upgrade,
-        positions.category AS position_category,
-        profiles.rank, profiles.full_name
+        profiles.rank, profiles.full_name,
+        scenario_completion.percentage
       FROM user_positions
-      JOIN positions ON user_positions.position = positions.position
-      JOIN profiles ON user_positions.user_id = profiles.user_id
+      JOIN positions ON positions.position = user_positions.position
+      JOIN profiles ON profiles.user_id = user_positions.user_id
+      JOIN scenario_completion ON scenario_completion.user_position_id = user_positions.id
       WHERE end_date IS NULL AND approval_date IS NULL;
     `;
     let result = await client.query(query);
+    const trainees = result.rows;
 
     if (result.rowCount === 0) {
       console.log("No trainees found");
@@ -42,8 +49,7 @@ const getTrainees = async (req: InstructorRequest, res: Response) => {
       return;
     }
 
-    const trainees = result.rows;
-    const data: { trainees: any; access?: string } = { trainees };
+    const data: any = { trainees };
 
     if (req.newToken) {
       data.access = req.newToken;
@@ -53,6 +59,7 @@ const getTrainees = async (req: InstructorRequest, res: Response) => {
     res.json({ status: "ok", message: "Retrieved all trainees", data });
   } catch (err: any) {
     console.log(err);
+    await client.query("ROLLBACK;");
     res
       .status(400)
       .json({ status: "error", message: "Failed to get user positions" });
